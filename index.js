2d34c592300ba6252342366a7d63aa0120d2a607
Arshdeepsustainabilityeconomics <arshdeep@sustainabilityeconomics.com>
2025-01-03 00:06:00 +0530

    Enabling of cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRoutes from './routes/userRoutes.js'; 
 import dotenv from 'dotenv';
+import cors from 'cors'; // Import the cors package
 
 dotenv.config();
 
@@ -9,12 +10,12 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors()); // Enable CORS
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected successfully'))
     .catch((err) => console.error('MongoDB connection error:', err));
-
 
 // Routes
 app.use('/api/users', userRoutes);
@@ -24,8 +25,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-
-
-
-
