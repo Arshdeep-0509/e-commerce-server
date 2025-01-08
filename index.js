074f3169a65b2b083bad88a38dc84d4df40ef463
sudhakar-se <sudhakar@sustainabilityeconomics.com>
2025-01-08 15:16:14 +0530

    feat: added oAuth

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import userRoutes from './routes/userRoutes.js'; 
-import dotenv from 'dotenv';
-import cors from 'cors'; // Import the cors package
+import express from "express";
+import mongoose from "mongoose";
+import passport from "passport";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import "../server/config/passportConfig.js"; 
 
 dotenv.config();
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
-app.use(cors()); // Enable CORS
+app.use(passport.initialize());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB connected successfully'))
-    .catch((err) => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Routes
-app.use('/api/users', userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
-// Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
